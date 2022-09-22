748926bc2cdd78c7ea7c9bcd102c9cd34b5cfb58
Camilla Rees <camilla.c.rees@gmail.com>
2022-09-21 19:46:43 -0600

    attempted lab 2 requirements

--- src/App.js
+++ src/App.js
@@ -20,11 +20,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-
-// The Main component needs to render at least two copies of a component called HornedBeast.
-
-// The Main component needs to pass title, imageUrl, and description into each HornedBeast component. For the purpose of today’s lab, you can pass whatever title, url and description that you want into each HornedBeast commponent.
-
-// The HornedBeast component needs to contain an <h2> that displays the title of the animal, an <img> element with src, alt and title attributes, and a <p> that displays the description.
-
