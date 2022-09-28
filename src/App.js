51ba092731bb5c74183829211e9ddb36ff2612f8
Camilla Rees <camilla.c.rees@gmail.com>
2022-09-27 22:48:51 -0600

    complete lab 4 requirements

--- src/App.js
+++ src/App.js
@@ -1,57 +1,71 @@
 import React from 'react';
 import './App.css';
-// import HornedBeast from './HornedBeast.js';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 import SelectedBeast from './SelectedBeast.js';
 import data from './data.json';
 
+
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      data: data,
       showModal: false,
-      beastObj: {}
+      beastObj: {},
+// create a new array called allBeasts that carries the data from json
+      allBeasts: data
     }
   }
 
-  handleCloseModal = () => {
-    this.setState({ showModal: false });
+// create function that passes new beasts array created in main
+  filterBeastArray = filteredBeastsArray => {
+// change the state allBeasts array to the filteredBeastsArray so it's no longer carrying data from json and can be altered
+    this.setState({allBeasts: filteredBeastsArray})
   }
 
-  handleShowModal = (beastTitle) => {
-    this.setState({
-      showModal: true,
-      beastObj: beastTitle,
-    });
-  }
-  render() {
-    return (
-      <>
-        <div className="App">
+    handleCloseModal = () => {
+      this.setState({ showModal: false });
+    }
 
-          <Header />
-        </div>
-        <div>
-          <SelectedBeast
-            show={this.state.showModal}
-            handleClose={this.handleCloseModal}
-            beastObj={this.state.beastObj}
-          />
-        </div>
-        <div>
-          <Main data={data}
-            handleShowModal={this.handleShowModal} />
-        </div>
-        <div>
-          <Footer />
-        </div>
-      </>
-    );
-  }
-}
+    handleShowModal = (beastTitle) => {
+      this.setState({
+        showModal: true,
+        beastObj: beastTitle,
+      });
+    }
+    render() {
+      return (
+        <>
+          <div className="App">
 
-export default App;
+            <Header />
+              </div>
+              <div>
+                <SelectedBeast
+                  show={this.state.showModal}
+                  handleClose={this.handleCloseModal}
+                  beastObj={this.state.beastObj}
+                />
+              </div>
+              <div>
+                <Main 
+                // pass the state of the allBeasts array to Main
+                  data={this.state.allBeasts}
+                  handleShowModal={this.handleShowModal}
+                  hornedBeasts={this.state.beastStats}
+                  handleSelectedHorns={this.handleSelectedHorns}
+                // pass the function that changes state to Main
+                  filterBeastArray={this.filterBeastArray}
+                   />
+              </div>
+              <div>
+                <Footer />
+              </div>
+            </>
+            );
+    }
+  }
+
+            export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,29 +1,70 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-// import SelectedBeast from './SelectedBeast';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import data from './data.json';
 
 class Main extends React.Component {
+
+    // create function that handles the events of a selection from the dropdown form
+    handleSelectedHorns =  event => {
+        const hornsSelected = event.target.value;
+    // create changeable array which state can updated when passed through from App.js props
+        let filteredBeastsArray;
+    
+        if (hornsSelected === "Any") {
+          this.setState({ allBeasts: data});
+          } else if (hornsSelected === "1") {
+            filteredBeastsArray = data.filter(beast => beast.horns === 1);
+            // alter filtered beast array state through props from function in App.js
+            this.props.filterBeastArray(filteredBeastsArray)
+          } else if (hornsSelected === "2") {
+            filteredBeastsArray = data.filter(beast => beast.horns === 2);
+            this.props.filterBeastArray(filteredBeastsArray)
+        } else if (hornsSelected === "3") {
+          filteredBeastsArray = data.filter(beast => beast.horns === 3);
+          this.props.filterBeastArray(filteredBeastsArray)
+        }  else if (hornsSelected === "100") {
+          filteredBeastsArray = data.filter(beast => beast.horns === 100);
+          this.props.filterBeastArray(filteredBeastsArray)
+    
+        };
+        console.log(filteredBeastsArray);
+      };
+
     render() {
         return (
-            <Container className="Main">
-                {this.props.data.map(beastObj => (
-                    <><HornedBeast
-                        title={beastObj.title}
-                        imageUrl={beastObj.image_url}
-                        description={beastObj.description}
-                        showModal={this.props.handleShowModal}
-                        beastObj={beastObj} />
-                        {/* <SelectedBeast
-                
-                beast={beastObj}
-                /> */}
-                    </>
-                ))}
-
-            </Container>
-        )
+            <>
+            <Container>
+                <Form>
+                    <Form.Group controlId="selectedNumberOfHorns" >
+                        <Form.Label>Filter by number of horns</Form.Label>
+                        <Form.Control
+                            as="select"
+                            onChange={this.handleSelectedHorns}>
+                        <option value="Any">Any</option>
+                            <option value="1">1</option>
+                                <option value="2">2</option>
+                                    <option value="3">3</option>
+                                        <option value="100">100</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                </Form>
+                            </Container>
+                            <Container className="Main">
+                                {this.props.data.map(beastObj => (
+                                    <HornedBeast
+                                        title={beastObj.title}
+                                        imageUrl={beastObj.image_url}
+                                        description={beastObj.description}
+                                        showModal={this.props.handleShowModal}
+                                        beastObj={beastObj} />
+                                
+                                ))}
+                            </Container>
+                            </>
+                            )
     }
 };
 
-export default Main;
+                            export default Main;
